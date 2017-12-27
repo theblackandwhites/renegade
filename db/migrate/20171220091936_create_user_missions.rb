f9b602268706172f2bc760e8f565ab011ec7726e
Blackhat Digital <josh@blackhatdigital.com.au>
2017-12-27 16:14:18 +1000

    initial deploy

--- db/migrate/20171220091936_create_user_missions.rb
+++ db/migrate/20171220091936_create_user_missions.rb
@@ -1,7 +1,7 @@
 class CreateUserMissions < ActiveRecord::Migration[5.0]
   def change
     create_table :user_missions do |t|
-      t.references :user, foreign_key: true
+      #t.references :user, foreign_key: true
       t.references :mission, foreign_key: true
 
       t.timestamps
